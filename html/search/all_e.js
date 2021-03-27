4d71d06d8d9eb3f013c1cdeee3624c4bb99400d7
michele-bertoni <michele2.bertoni@mail.polimi.it>
2021-03-27 11:20:40 +0100

    Adding doxygen documentation

--- html/search/all_e.js
+++ html/search/all_e.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['odometry_83',['odometry',['../class_omni3.html#a590b4e7a6f29454f1594cd2d283555d9',1,'Omni3']]],
-  ['omni3_84',['Omni3',['../md__c___users_mikib__documents__arduino_libraries_omni3__r_e_a_d_m_e.html',1,'(Global Namespace)'],['../class_omni3.html',1,'Omni3'],['../class_omni3.html#a26cad2c82dc4ba0c8aa0c25d78151c89',1,'Omni3::Omni3(Wheel *rightWheel, Wheel *backWheel, Wheel *leftWheel, Movements *movements, omni3_params_t parameters)'],['../class_omni3.html#a521ad9fda5ef50ba2ed8e1cd77d2bf5e',1,'Omni3::Omni3(Wheel *rightWheel, Wheel *backWheel, Wheel *leftWheel, Movements *movements, int memAddr)']]],
+  ['omni3_84',['Omni3',['../index.html',1,'(Global Namespace)'],['../class_omni3.html',1,'Omni3'],['../class_omni3.html#a26cad2c82dc4ba0c8aa0c25d78151c89',1,'Omni3::Omni3(Wheel *rightWheel, Wheel *backWheel, Wheel *leftWheel, Movements *movements, omni3_params_t parameters)'],['../class_omni3.html#a521ad9fda5ef50ba2ed8e1cd77d2bf5e',1,'Omni3::Omni3(Wheel *rightWheel, Wheel *backWheel, Wheel *leftWheel, Movements *movements, int memAddr)']]],
   ['omni3_2ecpp_85',['omni3.cpp',['../omni3_8cpp.html',1,'']]],
   ['omni3_2eh_86',['omni3.h',['../omni3_8h.html',1,'']]],
   ['omni3_5fparams_5fs_87',['omni3_params_s',['../structomni3__params__s.html',1,'']]],
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['omni3_304',['Omni3',['../md__c___users_mikib__documents__arduino_libraries_omni3__r_e_a_d_m_e.html',1,'']]]
+  ['omni3_304',['Omni3',['../index.html',1,'']]]
 ];
